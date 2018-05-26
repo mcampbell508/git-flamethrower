3a028b2e1a6146972f8cf603a205dcac76f5ea2a
Matt Campbell <matthewcampbell508@gmail.com>
2018-05-26 13:10:14 +0100

    Remove `src` folder

--- src/Console/Application.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace MCampbell508\Git\Flamethrower\Console;
-
-use Symfony\Component\Console\Application as SymonfyApplication;
-
-class Application extends SymonfyApplication
-{
-}
--- src/Console/CLImate.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace MCampbell508\Git\Flamethrower\Console;
-
-use League\CLImate\CLImate as PHPLeagueCLImate;
-
-class CLImate extends PHPLeagueCLImate
-{
-}
