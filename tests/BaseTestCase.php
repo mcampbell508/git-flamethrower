8eda3b1d708f292826c34c96bc89138615f22cbb
Matt Campbell <matt.campbell@theshopworks.com>
2018-07-10 16:34:11 +0100

    Add stock Laravel Zero application
    This was created using the Laravel Zero installer. It is a basic
    skeleton of the project application.

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Shopworks\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTestCase extends TestCase
+{
+}
---
+++ tests/CreatesApplication.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Shopworks\Tests;
+
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use LaravelZero\Framework\Kernel;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application and returns it.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application
+     */
+    public function createApplication(): ApplicationContract
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
