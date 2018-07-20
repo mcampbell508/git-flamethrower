dc911bd939c23fc74b590b44997b2c3110a0e924
Matt Campbell <matt.campbell@theshopworks.com>
2018-07-20 13:39:53 +0100

    Add Configuration repository
    This will behave much as the same as Laravel's config repository, where
    we can retrieve values using the dot notation.
    
    This will be useful when working with the YML configuration options
    that this package provides.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopworks\Git\Review\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Shopworks\Git\Review\Yml\YmlConfiguration;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->bind(YmlConfiguration::class, function () {
+            return new YmlConfiguration(\getcwd());
+        });
     }
 }
